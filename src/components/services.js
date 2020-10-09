6380c70121da5a28d19c321df431a0a0ff00166f
otsolap <59653348+otsolap@users.noreply.github.com>
2020-10-09 15:07:42 +0300

    added to .toml file my functions folder and refactored the services component.

--- src/components/services.js
+++ src/components/services.js
@@ -1,46 +1,11 @@
 import React from "react"
-import { useEffect, useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import {
-  Elements,
-  PaymentRequestButtonElement,
-  useStripe,
-} from '@stripe/react-stripe-js';
 import inventory from '../../static/inventory/services.json';
 
 const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
 
 const Services = () => {
-  const stripe2 = useStripe();
-  const [paymentRequest, setPaymentRequest] = useState(null);
-
-  useEffect(() => {
-    if (!stripe2 || paymentRequest !== null) {
-      return;
-    }
-
-    const request = stripe2.paymentRequest({
-      country: 'FI',
-      currency: 'eur',
-      total: {
-        label: 'Kultakämmen palvelut',
-        amount: 99900,
-      },
-      requestPayerName: false,
-      requestPayerEmail: false,
-    });
-
-    request.canMakePayment().then((result) => {
-      console.log({ result });
-      if (result) {
-        setPaymentRequest(request);
-      }
-    });
-  }, [stripe2, paymentRequest]);
-
   const format = (amount, currency) =>
-    // Intl.NumberFormat MDN: 
-    // Konstrukti, joka muokkaa numeron kieliformaatin perusteella.
     new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: currency,
@@ -48,17 +13,13 @@ const Services = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // Formdatasta saadaan formaatin data irti
-    // eli me saamme tuotteen tiedot irti tästä.
-    // Niin checkouttiin tulee oikeat datat.
     const form = new FormData(event.target);
 
     const data = {
       sku: form.get('sku'),
-      quantity: Number(form.get('quantity')),
     };
 
-    // Netlify cloud-funktio!
+    // TODO send to serverless function
     const response = await fetch('/.netlify/functions/create-checkout', {
       method: 'POST',
       headers: {
@@ -66,10 +27,10 @@ const Services = () => {
       },
       body: JSON.stringify(data),
     }).then((res) => res.json());
-    // ottaa sessionID 
-    // jonka tarvitsemme redirect
-    // checkouttiin.
+
+    // TODO get the session ID and redirect to checkout
     const stripe = await stripePromise;
+
     const { error } = await stripe.redirectToCheckout({
       sessionId: response.sessionId,
     });
@@ -85,7 +46,7 @@ const Services = () => {
         mt: 5,
         '@media (min-width: 360px)': {
           display: 'grid',
-          gap: 2,
+          gap: 3,
           gridTemplateColumns: 'repeat(2, 1fr)',
         },
         '@media (min-width: 500px)': {
@@ -93,13 +54,6 @@ const Services = () => {
         },
       }}
     >
-      {paymentRequest && (
-        <p>
-          Osta
-          <PaymentRequestButtonElement options={{ paymentRequest }} />
-        </p>
-      )}
-
       {inventory.map((service) => (
         <div key={service.sku} sx={{ mt: 3 }}>
           <img
@@ -131,12 +85,12 @@ const Services = () => {
               sx={{ fontSize: 1, fontWeight: 600, p: 2, textAlign: 'right' }}
             >
               Quantity
-            </label>
+          </label>
             <input
               type="number"
               id="quantity"
               name="quantity"
-              value={1}
+              defaultValue={1}
               min="1"
               max="10"
               sx={{
@@ -160,16 +114,16 @@ const Services = () => {
                 gridColumn: '1 / 3',
                 p: 2,
                 textShadow: `
-                  0.05em 0.05em #4F4F4F99,
-                  0.05em -0.05em #4F4F4F99,
-                  -0.05em 0.05em #4F4F4F99,
-                  -0.05em -0.05em #4F4F4F99
-                `,
+                0.05em 0.05em #4F4F4F99,
+                0.05em -0.05em #4F4F4F99,
+                -0.05em 0.05em #4F4F4F99,
+                -0.05em -0.05em #4F4F4F99
+              `,
                 textTransform: 'uppercase',
               }}
             >
               Buy Now
-            </button>
+          </button>
           </form>
         </div>
       ))}
@@ -177,12 +131,4 @@ const Services = () => {
   );
 };
 
-const WrappedServices = () => {
-  return (
-    <Elements stripe={stripePromise}>
-      <Services />
-    </Elements>
-  );
-};
-
-export default WrappedServices;
+export default Services;
