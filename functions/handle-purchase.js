7aede301cb9ac0ce54fe885b141c9652223d9bb5
otsolap <59653348+otsolap@users.noreply.github.com>
2020-10-09 14:19:41 +0300

    Testing the handle-purchase function on live.

--- functions/handle-purchase.js
+++ functions/handle-purchase.js
@@ -4,8 +4,12 @@ const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 exports.handler = async ({ headers, body }) => {
   const sig = headers['stripe-signature'];
   let event;
+
   try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(
+      body,
+      sig,
+      endpointSecret);
   } catch (error) {
     return {
       statusCode: 400,
@@ -15,16 +19,13 @@ exports.handler = async ({ headers, body }) => {
   // Stripesta otettu
   if (event.type === 'checkout.session.completed') {
     const session = event.data.object;
-
     console.log(session);
   }
-
   // TODO read out the line items
   // TODO send email - Hanki Sendgrid APi, jotta saat dataa.
   // Front-end mastercourse JamStack, selvitä sieltä.
-
   return {
     statusCode: 200,
-    body: JSON.stringify({ sessionId: session.id }),
+    body: JSON.stringify({ received: true }),
   };
 };
