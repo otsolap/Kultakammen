714b69a12c924320ef8281b7b3c2db6756f1d4d9
OtsoLap <otsolappalainen@gmail.com>
2021-11-10 22:50:53 +0200

    testing netlify doc code.

--- serverless/update-role/update-role.js
+++ serverless/update-role/update-role.js
@@ -1,40 +1,20 @@
 const fetch = require('node-fetch')
 
-exports.handler = async (event, context) => {
+exports.handler = function (event, context, callback) {
+  const data = JSON.parse(event.body);
+  const { user } = data;
 
-  // When you make a request to a Netlify Function using identity.authorizedFetch()
-  // client-side, context.clientContext becomes available in the function (here),
-  // and contains the user object, which is the verified, authorized user that is
-  // calling the function. This is validated with the JWT signing key used by GoTrue
-  // so if the .user object is there, we can know for certain that the calling client
-  // is indeed the authorized user. Yay!
-  const { user, identity } = context.clientContext
-
-  const userUrl = `${identity.url}/admin/users/${user.sub}`
-  const adminAuthorization = `Bearer ${identity.token}`
-
-  const payload = {
+  const responseBody = {
     app_metadata: {
-      ...user.app_metadata,
-      roles: ["user"]
-    }
-  }
-
-  // Conversely to above, we can leverage the 'admin' abilities granted to Functions
-  // to make admin-level changes to users in our Netlify Identity instance. This is
-  // similar to logging into the Netlify UI and manually adding or removing a role.
-  //
-  // See this thread for more info on what the 'admin' context means:
-  // https://community.netlify.com/t/questions-about-netlify-identity-serverless-functions/20755/13?u=jonsully
-  await fetch(userUrl, {
-    method: 'PUT',
-    headers: {
-      Authorization: adminAuthorization,
+      roles: ["user"],
     },
-    body: JSON.stringify(payload)
-  })
-
-  return {
-    statusCode: 200
-  }
-}
+    user_metadata: {
+      ...user.user_metadata, // append current user metadata
+      custom_data_from_function: "hurray this is some extra metadata"
+    }
+  };
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(responseBody)
+  });
+};
