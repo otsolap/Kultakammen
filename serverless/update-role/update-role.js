7d6a8c325f926fe40d367415a20af803b56b4015
OtsoLap <otsolappalainen@gmail.com>
2021-11-10 22:35:39 +0200

    testing serverless.

--- serverless/update-role/update-role.js
+++ serverless/update-role/update-role.js
@@ -9,20 +9,14 @@ exports.handler = async (event, context) => {
   // so if the .user object is there, we can know for certain that the calling client
   // is indeed the authorized user. Yay!
   const { user, identity } = context.clientContext
-  const { action, role } = JSON.parse(event.body)
 
   const userUrl = `${identity.url}/admin/users/${user.sub}`
   const adminAuthorization = `Bearer ${identity.token}`
 
-  const currentRoles = user.app_metadata.roles || []
-  const newRoles = action === 'add'
-    ? currentRoles.concat(role)
-    : currentRoles.filter(r => r !== role)
-
   const payload = {
     app_metadata: {
       ...user.app_metadata,
-      roles: newRoles
+      roles: ["user"]
     }
   }
 
